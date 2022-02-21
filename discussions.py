79a7ed8ca18ade5213adafe27af7fdb22be6e9b2
Webs <weberswords@gmail.com>
2022-02-21 15:13:52 -0800

    Pull questions tagged r

--- discussions.py
+++ discussions.py
@@ -3,5 +3,5 @@ import json
 SITE = StackAPI('stackoverflow')
 
 with open('comment_file.json', "x+") as file:
-    comments = SITE.fetch('comments')
-    file.write(json.dumps(comments))
+    questions = SITE.fetch('questions', order="desc", tagged="r", sort="hot")
+    file.write(json.dumps(questions))
