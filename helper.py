38383afcc0de0936e5f13ed113ff8afaead476b6
Webs <weberswords@gmail.com>
2022-03-14 10:40:11 -0700

    Add sorting by views

--- helper.py
+++ helper.py
@@ -1,5 +1,24 @@
 import json
+import os
 
 def write_to_json_file(data, file_name):
-    with open(file_name, 'x+') as file:
-        file.write(json.dumps(data))
+    try:
+        print("Removing existing file...")
+        os.remove(file_name)
+        print(f"{file_name} removed.")
+        print("Writing new data...")
+        with open(file_name, 'x+') as file:
+            file.write(json.dumps(data))
+    except FileNotFoundError:
+        print("Writing new data...")
+        with open(file_name, 'x+') as file:
+            file.write(json.dumps(data))
+   
+def get_key_by_views(obj):
+    return obj['view_count']
+
+def sort_by_views(list):
+    print(f"Before: {list}")
+    list.sort(key=get_key_by_views)
+    print(f"After: {list}")
+
