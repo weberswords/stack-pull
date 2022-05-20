e5503628b7565168bf4e498f5fb09a0a7e430e44
webs <weberswords@gmail.com>
2022-05-20 14:53:59 -0700

    Separate auth into separate file

---
+++ auth.py
@@ -0,0 +1,14 @@
+import json
+from stackapi import StackAPI
+
+def get_site():
+    with open('secrets.json', 'r') as raw_secrets:
+        secrets = json.load(raw_secrets)
+        key = secrets.get('key')
+        access_token = secrets.get('access_token')
+    SITE = StackAPI('stackoverflow', key=key, access_token=access_token)
+    return SITE
+
+def test_connection():
+    response = SITE.fetch('me')
+    print(response)
